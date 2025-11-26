6b7b5eac7ddaeae61581205693f01de42f3438ac
minjakekwon <james970506@gmail.com>
2025-11-26 17:24:28 +0900

    feat : create 메시징 변경 (#6)

--- src/main/java/org/pokeherb/deliveryservice/infrastructure/messaging/event/DeliveryCreatedHandler.java
+++ src/main/java/org/pokeherb/deliveryservice/infrastructure/messaging/event/DeliveryCreatedHandler.java
@@ -1,11 +1,35 @@
 package org.pokeherb.deliveryservice.infrastructure.messaging.event;
 
+import lombok.extern.slf4j.Slf4j;
+import org.pokeherb.deliveryservice.application.command.DeliveryCommandService;
+import org.pokeherb.deliveryservice.application.service.request.DeliveryCreateRequestDto;
+import org.pokeherb.deliveryservice.infrastructure.messaging.messageHandler.AbstractDeliveryEventHandler;
 import org.springframework.stereotype.Component;
 
+;
+
+@Slf4j
 @Component("delivery.created")
-public class DeliveryCreatedHandler implements DeliveryEventHandler {
-    @Override
+public class DeliveryCreatedHandler extends AbstractDeliveryEventHandler {
+
+    private final DeliveryCommandService deliveryCommandService;
+
+    public DeliveryCreatedHandler(
+            com.fasterxml.jackson.databind.ObjectMapper objectMapper,
+            DeliveryCommandService deliveryCommandService
+    ) {
+        super(objectMapper);
+        this.deliveryCommandService = deliveryCommandService;
+    }
+
     public void handle(String payload) {
-        // created 로직
+        // 공통 메서드로 JSON → DTO 변환
+        DeliveryCreateRequestDto dto =
+                readPayload(payload, DeliveryCreateRequestDto.class);
+
+        // 도메인 서비스 호출
+        deliveryCommandService.createDelivery(dto);
+
+        log.info("Delivery created via MQ event: orderId={}", dto.orderId());
     }
 }
