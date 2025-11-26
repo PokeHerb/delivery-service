a4f2c7866dbe6924ddbdce71b06c90b34bb732eb
minjakekwon <james970506@gmail.com>
2025-11-26 17:28:45 +0900

    feat : 딜리버리 레포 변경 (#6)

--- src/main/java/org/pokeherb/deliveryservice/domain/repository/DeliveryRepository.java
+++ src/main/java/org/pokeherb/deliveryservice/domain/repository/DeliveryRepository.java
@@ -1,7 +1,6 @@
 package org.pokeherb.deliveryservice.domain.repository;
 
 import org.pokeherb.deliveryservice.domain.entity.Delivery;
-import org.pokeherb.deliveryservice.domain.entity.DeliveryStatus;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -10,6 +9,4 @@ public interface DeliveryRepository{
     Delivery save(Delivery delivery);
 
     Optional<Delivery> findById(UUID deliveryId);
-
-    Optional<Delivery> findByDeliveryStatus(DeliveryStatus status);
 }
